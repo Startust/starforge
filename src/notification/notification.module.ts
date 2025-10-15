5ac4d8d79ac859ccd6f7108f6d21210015743d87
Yihui Liu <yl2900@nyu.edu>
2025-10-14 21:58:25 -0400

    Set Prisma and Notification modules to global

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { GmailService } from './gmail.service';
 import { NotificationService } from './notification.service';
 
+@Global()
 @Module({
   providers: [NotificationService, GmailService],
   exports: [NotificationService, GmailService],
